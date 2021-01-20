6c30d27679a9752e29dbe14b5f6246494f561ce5
v1690117 <v1690117@outlook.com>
2021-01-20 08:37:46 +0300

    Security was disabled temporary for dev purposes

--- src/main/java/com/v1690117/rutt/security/SecurityConfig.java
+++ src/main/java/com/v1690117/rutt/security/SecurityConfig.java
@@ -1,25 +1,25 @@
 package com.v1690117.rutt.security;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.core.userdetails.User;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
-@Configuration
-@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    @Bean
-    @Override
-    protected UserDetailsService userDetailsService() {
-        UserDetails user = User.withDefaultPasswordEncoder()
-                .username("user")
-                .password("pass")
-                .roles("USERS")
-                .build();
-        return new InMemoryUserDetailsManager(user);
-    }
+//@Configuration
+//@EnableWebSecurity
+public class SecurityConfig {// extends WebSecurityConfigurerAdapter {
+//    @Bean
+//    @Override
+//    protected UserDetailsService userDetailsService() {
+//        UserDetails user = User.withDefaultPasswordEncoder()
+//                .username("user")
+//                .password("pass")
+//                .roles("USERS")
+//                .build();
+//        return new InMemoryUserDetailsManager(user);
+//    }
 }
