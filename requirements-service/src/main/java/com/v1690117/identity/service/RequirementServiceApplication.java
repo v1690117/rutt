b80437e774cd031edf326c32b155bbf577921e78
v1690117 <v1690117@outlook.com>
2022-05-03 22:29:06 +0300

    Requirements module was inited

---
+++ requirements-service/src/main/java/com/v1690117/identity/service/RequirementServiceApplication.java
@@ -0,0 +1,11 @@
+package com.v1690117.identity.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequirementServiceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(RequirementServiceApplication.class, args);
+	}
+}
