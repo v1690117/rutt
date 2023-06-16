6251fdcf9d22b12504ab7ffe28288944cdb1fd4f
v1690117 <v1690117@outlook.com>
2023-06-16 23:47:44 +0200

    make it a little closer to design

--- frontend/src/components/Button/Button.styles.ts
+++ frontend/src/components/Button/Button.styles.ts
@@ -2,19 +2,19 @@ import styled from 'styled-components';
 
 export const ButtonWrapper = styled.div<{ type: 'positive' | 'negative' }>`
   background-color: ${props => props.type === 'positive'
-    ? props.theme.bgColors.main
-    : props.theme.bgColors.second};
+          ? props.theme.bgColors.main
+          : props.theme.bgColors.second};
   color: ${props => props.type === 'positive'
-    ? props.theme.btnTextColors.main
-    : props.theme.btnTextColors.second};
+          ? props.theme.btnTextColors.main
+          : props.theme.btnTextColors.second};
   border: 1px solid ${props => props.theme.colors.main};
-  box-shadow: 2px 2px 5px ${props => props.theme.rgbaColors.main}; 
+  box-shadow: 2px 2px 5px ${props => props.theme.rgbaColors.main};
   display: inline-block;
   box-sizing: border-box;
-  padding: 4px 16px; 
+  padding: 4px 16px;
   height: 37px;
   border-radius: 10px;
-  font-family: 'Inter', sans-serif;
+  font-family: Inter, sans-serif;
   font-style: normal;
   font-weight: 400;
   font-size: 24px;
@@ -22,5 +22,5 @@ export const ButtonWrapper = styled.div<{ type: 'positive' | 'negative' }>`
   text-transform: capitalize;
   text-align: center;
   vertical-align: middle;
-  cursor: pointer;   
+  cursor: pointer;
 `;
