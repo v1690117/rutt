af0ea60eddc39e95f3d20878bd85eafcf776516c
v1690117 <v1690117@outlook.com>
2023-07-01 02:18:50 +0200

    cleanup

--- tests-service/src/main/kotlin/com/rutt/testsservice/security/SecurityConfig.kt
+++ tests-service/src/main/kotlin/com/rutt/testsservice/security/SecurityConfig.kt
@@ -3,15 +3,13 @@ package com.rutt.testsservice.security
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
-import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer
 import org.springframework.security.web.SecurityFilterChain
 
 @Configuration
 class SecurityConfig {
     @Bean
     fun defaultSecurityFilterChain(http: HttpSecurity): SecurityFilterChain? {
-        (http.authorizeRequests()
-            .anyRequest() as AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizedUrl).authenticated()
+        http.authorizeRequests().anyRequest().authenticated()
         http.formLogin()
         http.httpBasic()
         http.csrf().disable()
