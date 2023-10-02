997b4d989087a9afa8e3dde975d583415fe58750
v1690117 <v1690117@outlook.com>
2023-10-02 18:24:38 +0300

    fix api pathes and docker-compose

--- tests-service/src/main/kotlin/com/rutt/testsservice/api/SuiteAPI.kt
+++ tests-service/src/main/kotlin/com/rutt/testsservice/api/SuiteAPI.kt
@@ -5,24 +5,24 @@ import com.rutt.testsservice.domain.Suite
 import org.springframework.web.bind.annotation.*
 
 interface SuiteAPI {
-    @GetMapping("/suites")
+    @GetMapping("/api/suites")
     fun findAll(): List<Suite>;
 
-    @GetMapping("/suites/{id}")
+    @GetMapping("/api/suites/{id}")
     fun get(@PathVariable(name = "id") id: Long): Suite
 
-    @PostMapping("/suites")
+    @PostMapping("/api/suites")
     fun create(@RequestBody suite: Suite): Suite;
 
-    @PostMapping("/suites/{id}/cases")
+    @PostMapping("/api/suites/{id}/cases")
     fun addNewCases(@PathVariable(name = "id") suiteId: Long, @RequestBody cases: List<Case>): Suite
 
-    @PutMapping("/suites/{id}/cases")
+    @PutMapping("/api/suites/{id}/cases")
     fun addExistingCases(@PathVariable(name = "id") suiteId: Long, @RequestBody caseIds: List<Long>): Suite
 
-    @PutMapping("/suites/{id}")
+    @PutMapping("/api/suites/{id}")
     fun update(@PathVariable id: Long, @RequestBody newSuite: Suite): Suite
 
-    @DeleteMapping("/suites/{id}")
+    @DeleteMapping("/api/suites/{id}")
     fun deleteCase(@PathVariable(name = "id") id: Long)
 }
